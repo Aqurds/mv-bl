024131f1e1979fbbfaab408481eb4691cbbbca4b
aqurds <omarf1320@gmail.com>
2023-06-05 20:21:55 +0600

    Update Css, Js to fix Stylelint & ESLint error V1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,78 @@
 if (localStorage.getItem('Added Books') === null) {
-    localStorage.setItem('Added Books', JSON.stringify([]));
-  }
+  localStorage.setItem('Added Books', JSON.stringify([]));
+}
   
-  const storeData = JSON.parse(localStorage.getItem('Added Books'));
+const storeData = JSON.parse(localStorage.getItem('Added Books'));
   
-  function updateData() {
-    localStorage.setItem('Added Books', JSON.stringify(storeData));
-  }
+// Getting values from input fields
+const form = document.querySelector('#book-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const titleInput = document.querySelector('.book-title');
+  const authorInput = document.querySelector('.book-author');
+  addNewData(titleInput.value, authorInput.value);
+  titleInput.value = ''; 
+  authorInput.value = ''; 
+});
+
   
-  const form = document.querySelector('#book-form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const titleInput = document.querySelector('.book-title');
-    const authorInput = document.querySelector('.book-author');
-    addNewData(titleInput.value, authorInput.value);
-    titleInput.value = ''; 
-    authorInput.value = ''; 
-  });
-  
-  function createBooks(arr) {
-    let books = '';
-    for (let i = 0; i < arr.length; i += 1) {
-      books += `
-        <p>${arr[i].title}</p>
-        <p>${arr[i].author}</p>
-        <button onclick="removeBook(${i})">Remove</button>
-        <hr/>
-      `;
-    }
-    return books;
-  }
-  
-  function displayBooks() {
-    const listOfBooks = document.querySelector('.container-section');
-    listOfBooks.innerHTML = `
-      <ul class="book-ul">
-        ${createBooks(storeData)}
-      </ul>
+function createBooks(arr) {
+  let books = '';
+  for (let i = 0; i < arr.length; i += 1) {
+    books += `
+      <p>${arr[i].title}</p>
+      <p>${arr[i].author}</p>
+      <button onclick="removeBook(${i})">Remove</button>
+      <hr/>
     `;
   }
+  return books;
+}
   
-  function addNewData(titleBook, authorBook) {
-    const book = {
-      title: titleBook,
-      author: authorBook
-    };
-    storeData.push(book);
-    updateData();
-    displayBooks();
-  }
+function displayBooks() {
+  const listOfBooks = document.querySelector('.container-section');
+  listOfBooks.innerHTML = `
+    <ul class="book-ul">
+      ${createBooks(storeData)}
+    </ul>
+  `;
+}
+
+// Displaying data in the container
+function displayBooks() {
+  const listOfBooks = document.querySelector('.container-section');
+  listOfBooks.innerHTML = `
+    <ul class="book-ul">
+      ${createBooks(storeData)}
+    </ul>
+  `;
+}
+
+// Adding new data to local storage
+function addNewData(bookTitle, bookAuthor) {
+  const book = {
+    title: bookTitle,
+    author: bookAuthor
+  };
+  storeData.push(book);
+  updateData();
+  displayBooks();
+}
   
-  function removeBook(i) {
-    storeData.splice(i, 1);
-    updateData();
-    displayBooks();
-  }
-  
-  window.onload = displayBooks;
+function addNewData(titleBook, authorBook) {
+  const book = {
+    title: titleBook,
+    author: authorBook
+  };
+  storeData.push(book);
+  updateData();
+  displayBooks();
+}
+
+function removeBook(i) {
+  storeData.splice(i, 1);
+  updateData();
+  displayBooks();
+}
+
+window.onload = displayBooks;
