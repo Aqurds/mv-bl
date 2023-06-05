42d46e91fe476801e6fde77cae15dace169b9d03
aqurds <omarf1320@gmail.com>
2023-06-05 20:26:50 +0600

    sync

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,16 @@ if (localStorage.getItem('Added Books') === null) {
   localStorage.setItem('Added Books', JSON.stringify([]));
 }
   
+<<<<<<< HEAD
 const storeData = JSON.parse(localStorage.getItem('Added Books'));
+=======
+// Retrieving data from local storage
+const storeData = JSON.parse(localStorage.getItem('Added Books'));
+
+function updateData() {
+  localStorage.setItem('Added Books', JSON.stringify(storeData));
+}
+>>>>>>> f367a91 (Update Css, Js to fix Stylelint & ESLint error V1)
   
 // Getting values from input fields
 const form = document.querySelector('#book-form');
@@ -14,7 +23,10 @@ form.addEventListener('submit', (e) => {
   titleInput.value = ''; 
   authorInput.value = ''; 
 });
+<<<<<<< HEAD
 
+=======
+>>>>>>> f367a91 (Update Css, Js to fix Stylelint & ESLint error V1)
   
 function createBooks(arr) {
   let books = '';
@@ -28,6 +40,7 @@ function createBooks(arr) {
   }
   return books;
 }
+<<<<<<< HEAD
   
 function displayBooks() {
   const listOfBooks = document.querySelector('.container-section');
@@ -69,10 +82,39 @@ function addNewData(titleBook, authorBook) {
   displayBooks();
 }
 
+=======
+
+// Displaying data in the container
+function displayBooks() {
+  const listOfBooks = document.querySelector('.container-section');
+  listOfBooks.innerHTML = `
+    <ul class="book-ul">
+      ${createBooks(storeData)}
+    </ul>
+  `;
+}
+
+// Adding new data to local storage
+function addNewData(bookTitle, bookAuthor) {
+  const book = {
+    title: bookTitle,
+    author: bookAuthor
+  };
+  storeData.push(book);
+  updateData();
+  displayBooks();
+}
+  
+// Remove a book from local storage
+>>>>>>> f367a91 (Update Css, Js to fix Stylelint & ESLint error V1)
 function removeBook(i) {
   storeData.splice(i, 1);
   updateData();
   displayBooks();
 }
 
+<<<<<<< HEAD
 window.onload = displayBooks;
+=======
+window.onload = displayBooks;
+>>>>>>> f367a91 (Update Css, Js to fix Stylelint & ESLint error V1)
