bed8df641950030979e90f2d21dfcad438558b02
aqurds <omarf1320@gmail.com>
2023-06-07 16:29:38 +0600

    Update js file (Book class added)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,96 +1,174 @@
-// Check if there are any books in localStorage, and if not, initialize an empty array
-const bookCollection = JSON.parse(localStorage.getItem('bookCollection')) || [];
+// // Check if there are any books in localStorage, and if not, initialize an empty array
+// const bookCollection = JSON.parse(localStorage.getItem('bookCollection')) || [];
 
-// Function to update the data in localStorage
-function updateData() {
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-}
+// // Function to update the data in localStorage
+// function updateData() {
+//   localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+// }
 
-// Function to display books in the page
+// // Function to display books in the page
+// // function displayBooks() {
+// //   const bookList = document.querySelector('.container-section');
+// //   bookList.innerHTML = '';
+
+// //   bookCollection.forEach((book, index) => {
+// //     const bookItem = document.createElement('div');
+// //     bookItem.classList.add('book-item');
+
+// //     const titlePara = document.createElement('p');
+// //     titlePara.textContent = `Title: ${book.title}`;
+
+// //     const authorPara = document.createElement('p');
+// //     authorPara.textContent = `Author: ${book.author}`;
+
+// //     const removeButton = document.createElement('button');
+// //     removeButton.textContent = 'Remove';
+// //     removeButton.addEventListener('click', () => {
+// //       removeBook(index);
+// //     });
+
+// //     bookItem.appendChild(titlePara);
+// //     bookItem.appendChild(authorPara);
+// //     bookItem.appendChild(removeButton);
+// //     bookList.appendChild(bookItem);
+// //   });
+// // }
+
 // function displayBooks() {
-//   const bookList = document.querySelector('.container-section');
+//   const bookList = document.querySelector('.booklist-table');
 //   bookList.innerHTML = '';
 
 //   bookCollection.forEach((book, index) => {
-//     const bookItem = document.createElement('div');
+//     const bookItem = document.createElement('tr');
 //     bookItem.classList.add('book-item');
 
-//     const titlePara = document.createElement('p');
-//     titlePara.textContent = `Title: ${book.title}`;
+//     const titlePara = document.createElement('td');
+//     titlePara.textContent = `"${book.title}" by ${book.author}`;
 
-//     const authorPara = document.createElement('p');
-//     authorPara.textContent = `Author: ${book.author}`;
-
+//     const removeTd = document.createElement('td');
 //     const removeButton = document.createElement('button');
+//     removeButton.classList.add('btn');
+//     removeButton.classList.add('btn-light');
 //     removeButton.textContent = 'Remove';
 //     removeButton.addEventListener('click', () => {
 //       removeBook(index);
 //     });
 
+//     removeTd.appendChild(removeButton);
 //     bookItem.appendChild(titlePara);
-//     bookItem.appendChild(authorPara);
-//     bookItem.appendChild(removeButton);
+//     bookItem.appendChild(removeTd);
 //     bookList.appendChild(bookItem);
 //   });
 // }
 
-function displayBooks() {
-  const bookList = document.querySelector('.booklist-table');
-  bookList.innerHTML = '';
+// // Function to remove a book from the collection
+// function removeBook(index) {
+//   bookCollection.splice(index, 1);
+//   updateData();
+//   displayBooks();
+// }
 
-  bookCollection.forEach((book, index) => {
-    const bookItem = document.createElement('tr');
-    bookItem.classList.add('book-item');
+// // Function to add a new book to the collection
+// function addBook(title, author) {
+//   const book = {
+//     title,
+//     author,
+//   };
+//   bookCollection.push(book);
+//   updateData();
+//   displayBooks();
+// }
 
-    const titlePara = document.createElement('td');
-    titlePara.textContent = `"${book.title}" by ${book.author}`;
+// // Add event listener to the form
+// const form = document.querySelector('#book-form');
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const titleInput = document.querySelector('.book-title');
+//   const authorInput = document.querySelector('.book-author');
+//   addBook(titleInput.value, authorInput.value);
+//   titleInput.value = '';
+//   authorInput.value = '';
+// });
 
-    const removeTd = document.createElement('td');
-    const removeButton = document.createElement('button');
-    removeButton.classList.add('btn');
-    removeButton.classList.add('btn-light');
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => {
-      removeBook(index);
+// // Display the existing books on page load
+// document.addEventListener('DOMContentLoaded', () => {
+//   displayBooks();
+// });
+
+class Book {
+  constructor(formElem, bookCollectionName, elemToInsertData) {
+    this.bookCollectionName = bookCollectionName;
+    this.elemToInsertData = elemToInsertData;
+    this.formElem = document.querySelector(formElem);
+    this.bookCollection = JSON.parse(localStorage.getItem(this.bookCollectionName)) || [];
+    this.addEventHandler();
+    this.displayBooks();
+  }
+
+  updateData() {
+    localStorage.setItem(this.bookCollectionName, JSON.stringify(this.bookCollection));
+  }
+
+  addBook(title, author) {
+    if (!title || !author) {
+      alert('Please, insert title & author!')
+      return;
+    }
+    this.bookCollection.push({title, author});
+    this.updateData();
+    this.displayBooks();
+  }
+
+  removeBook(index) {
+    console.log("hello")
+    this.bookCollection.splice(index, 1);
+    this.updateData();
+    this.displayBooks();
+  }
+
+  generateHtmlForBookList() {
+    let boilerPlate = ''
+    this.bookCollection.forEach((book,index) => {
+      boilerPlate += `<tr class="book-item">
+        <td>"${book.title}" by ${book.author}</td>
+        <td>
+          <button class="btn btn-light" data-index=${index}>
+            Remove
+          </button>
+        </td>
+      </tr>`
+    })
+    return boilerPlate
+  }
+
+  activateRemoveButton() {
+    const table = document.querySelector('.booklist-table')
+    const btn = table.querySelectorAll('button')
+    console.log(btn)
+    btn.forEach((btn_r, index) => {
+      btn_r.addEventListener('click', () => {
+        this.removeBook(index)
+      })
+    })
+  }
+
+  displayBooks() {
+    const bookList = document.querySelector(this.elemToInsertData);
+    bookList.innerHTML = '';
+    bookList.innerHTML = this.generateHtmlForBookList()
+    this.activateRemoveButton();
+  }
+
+  addEventHandler() {
+    this.formElem.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const titleInput = document.querySelector('.book-title');
+      const authorInput = document.querySelector('.book-author');
+      this.addBook(titleInput.value, authorInput.value);
+      titleInput.value = '';
+      authorInput.value = '';
     });
-
-    removeTd.appendChild(removeButton);
-    bookItem.appendChild(titlePara);
-    bookItem.appendChild(removeTd);
-    bookList.appendChild(bookItem);
-  });
+  }
 }
 
-// Function to remove a book from the collection
-function removeBook(index) {
-  bookCollection.splice(index, 1);
-  updateData();
-  displayBooks();
-}
-
-// Function to add a new book to the collection
-function addBook(title, author) {
-  const book = {
-    title,
-    author,
-  };
-  bookCollection.push(book);
-  updateData();
-  displayBooks();
-}
-
-// Add event listener to the form
-const form = document.querySelector('#book-form');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const titleInput = document.querySelector('.book-title');
-  const authorInput = document.querySelector('.book-author');
-  addBook(titleInput.value, authorInput.value);
-  titleInput.value = '';
-  authorInput.value = '';
-});
-
-// Display the existing books on page load
-document.addEventListener('DOMContentLoaded', () => {
-  displayBooks();
-});
+const bookList = new Book("#book-form", "bookCollection", ".booklist-table")
