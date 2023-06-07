cdc4292bbdac575fa725ca827da1bf19b705dce2
aqurds <omarf1320@gmail.com>
2023-06-07 22:43:10 +0600

    Added Js for dynamic content section V2

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,11 +125,3 @@ triggerElem.forEach((singleElem, index) => {
   })
 })
 
-// Set date & time dynamically in "under navigation" & footer
-const dateTime = new Date()
-const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-const currentYear = dateTime.getFullYear();
-const currentMonth = monthList[dateTime.getMonth()];
-const currentDateTimeString = dateTime.toLocaleString().split('/')
-document.querySelector('#date-time').innerHTML = `${currentMonth} ${currentDateTimeString[1]} ${currentDateTimeString[2]}`;
-document.querySelector('#footer-year').innerHTML = currentYear;
