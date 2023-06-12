35f05bd9739c9c0ba669d2e00814f8db2b3baaba
aqurds <omarf1320@gmail.com>
2023-06-12 14:03:24 +0600

    Added date, time & year from luxon

--- assets/js/modules/date-time.js
+++ assets/js/modules/date-time.js
@@ -1,16 +1,9 @@
 import { luxon } from "./luxon.min.js";
 
 let dateTimeCustom = () => {
-  // Set date & time dynamically in "under navigation" & footer
-  const dateTime = new Date();
-  const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const currentYear = dateTime.getFullYear();
-  const currentMonth = monthList[dateTime.getMonth()];
-  const currentDateTimeString = dateTime.toLocaleString().split('/');
-  // document.querySelector('#date-time').innerHTML = `${currentMonth} ${currentDateTimeString[1]} ${currentDateTimeString[2]}`;
   document.querySelector('#date-time').innerHTML = luxon.DateTime.local().toLocaleString(luxon.DateTime.DATETIME_FULL_WITH_SECONDS)
 
-  document.querySelector('#footer-year').innerHTML = currentYear;
+  document.querySelector('#footer-year').innerHTML = luxon.DateTime.now().year;
 };
 
 export default dateTimeCustom;
